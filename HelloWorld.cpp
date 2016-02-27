2924f4846079d21b617e494ea7439ace146b6b2a
214068730 <siraaj.wilkonson1995@gmail.com>
2016-02-27 14:49:15 +0200

    HelloWorld commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout<<"Hello World!!!"<<endl;
+	cout<<"Hi my name is Siraaj WIlkinson"<<endl;
+	cout<<"I am in third year in group B";
+	return 0;
+}

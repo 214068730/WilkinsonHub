19218339bbe8fc3b3e65cf415149d48cb51962f0
Siraaj Wilkinson <siraaj.wilkonson1995@gmail.com>
2016-05-08 23:52:55 +0200

    assignment6_1

---
+++ assignment6_1/app/src/main/java/runningshop/assignment7/repository/shop/ItemRepository.java
@@ -0,0 +1,10 @@
+package runningshop.assignment7.repository.shop;
+
+import runningshop.assignment7.domain.shop.Item;
+import runningshop.assignment7.repository.Repository;
+
+/**
+ * Created by Siraaj on 04/20/2016.
+ */
+public interface ItemRepository extends Repository<Item,Long> {
+}
